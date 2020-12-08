c46a012280d925a7e51a787fbdafa7b962d62ac0
Novitz Jean-Philippe <novitz@gmail.com>
2020-12-08 21:07:50 +0100

    three articles with grey/green bullet

--- public/js/main.js
+++ public/js/main.js
@@ -10,4 +10,17 @@ triggerButton.forEach(btn => {
     })
 })
 
-console.log(mobileNav)
+
+/* Roll number bullet from grey to green */
+let numArticles = document.querySelectorAll('[id^="num-"]')
+let i =0
+numArticles.forEach(article => {
+    let num = article.querySelector('.num')
+    setTimeout(()=>{
+        num.style.backgroundColor = '#81b600'
+    }, i*4000)
+    setTimeout(()=>{
+        num.style.removeProperty('background-color')
+    }, i*8000)
+    i++
+})
